fb49a2c484116f45750fe8e30815d7a6cd165be2
Home <Home@Home-PC>
2015-04-07 19:44:53 -0300

    Print inserido

--- ProjetoHotelimar/src/projetohotelimar/ProjetoHotelimar.java
+++ ProjetoHotelimar/src/projetohotelimar/ProjetoHotelimar.java
@@ -17,6 +17,7 @@ public class ProjetoHotelimar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Teste de commit");
     }
     
 }
